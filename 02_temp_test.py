3b3cf6a71daf61e501414e3a1db52c5b2a8982ac
Shahd Sideek <s3660270@student.rmit.edu.au>
2018-08-14 21:48:48 +1000

    tutorial files

--- 02_temp_test.py
+++
@@ -1,7 +0,0 @@
-from sense_hat import SenseHat
-sense = SenseHat()
-sense.clear()
-
-temp = sense.get_temperature()
-sense.show_message('Temp: {0:0.1f} *c'.format(temp), scroll_speed=0.05)
-sense.clear()
